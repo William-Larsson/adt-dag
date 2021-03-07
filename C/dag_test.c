d1db83826930a6262596bb5b3bb04c82f692f052
TorVallin <c18tvn@cs.umu.se>
2021-03-07 16:49:33 +0100

    Tests, fixes etc.

--- C/dag_test.c
+++ C/dag_test.c
@@ -12,6 +12,7 @@ void test_longest_path(void);
 void test_longest_path_large(void);
 void test_topological_ordering(void);
 void test_small_topological_ordering(void);
+void test_topological_ordering_large(void);
 
 int main(void) {
     test_no_cycles();
@@ -21,7 +22,7 @@ int main(void) {
     test_longest_path();
     test_longest_path_large();
     test_small_topological_ordering();
-    test_topological_ordering();
+    test_topological_ordering_large();
     
     return 0;
 }
@@ -299,7 +300,7 @@ void test_longest_path_large(void) {
 
     if (*longPath != 51) 
         fprintf(stderr, "ERROR: test_longest_path_large: invalid longest path: %d\n", *longPath);
-        
+
     dag_destroy(d, false);
     free(longPath);
 }
@@ -335,39 +336,47 @@ void test_small_topological_ordering(void) {
     dag_destroy_path(l);
 }
 
-void test_topological_ordering(void) {
+// Same graph as test_longest_path_large
+void test_topological_ordering_large(void) {
     struct Dag *d = dag_create();
+    d->add = add_ints;
+    d->comp = int_compare;
 
-    int w1 = 1, w2 = 2, w3 = 3, w4 = 4, w5 = 5, w6 = 6, w7 = 7;
-    struct Vertex *A = dag_add_vertex(d, &w1);
-    struct Vertex *B = dag_add_vertex(d, &w2);
-    struct Vertex *C = dag_add_vertex(d, &w3);
-    struct Vertex *D = dag_add_vertex(d, &w4);
-    struct Vertex *E = dag_add_vertex(d, &w5);
-    struct Vertex *F = dag_add_vertex(d, &w6);
-    struct Vertex *G = dag_add_vertex(d, &w7);
+    int vw[] = {1, 2, 2, 6, 5, 15, 20, 25};
+    struct Vertex *A = dag_add_vertex(d, &vw[0]);
+    struct Vertex *B = dag_add_vertex(d, &vw[1]);
+    struct Vertex *C = dag_add_vertex(d, &vw[2]);
+    struct Vertex *D = dag_add_vertex(d, &vw[3]);
+    struct Vertex *E = dag_add_vertex(d, &vw[4]);
+    struct Vertex *F = dag_add_vertex(d, &vw[5]);
+    struct Vertex *G = dag_add_vertex(d, &vw[6]);
+    struct Vertex *H = dag_add_vertex(d, &vw[7]);
 
-    int res = 0;
-    res -= dag_add_edge(d, A, B, &w1); 
-    res -= dag_add_edge(d, B, E, &w1); 
-    res -= dag_add_edge(d, B, C, &w1); 
-    res -= dag_add_edge(d, B, D, &w1); 
-    res -= dag_add_edge(d, E, F, &w1); 
-    res -= dag_add_edge(d, F, G, &w1);  
+    int ew[] = {1, 2, 2, 5, 6, 3, 2, 7, 8, 4};
+    int res = dag_add_edge(d, A, B, &ew[0]); 
+    res -= dag_add_edge(d, A, D, &ew[1]); 
+    res -= dag_add_edge(d, B, C, &ew[2]); 
+    res -= dag_add_edge(d, B, D, &ew[3]); 
+    res -= dag_add_edge(d, B, E, &ew[4]); 
+    res -= dag_add_edge(d, C, E, &ew[5]); 
+    res -= dag_add_edge(d, C, H, &ew[6]); 
+    res -= dag_add_edge(d, D, E, &ew[7]); 
+    res -= dag_add_edge(d, E, F, &ew[8]); 
+    res -= dag_add_edge(d, E, G, &ew[9]); 
 
-    struct list *l = dag_topological_ordering(d);
-    struct node *it = list_first(l);
-
-    fprintf(stdout, "Order: ");
+    struct list *ordering = dag_topological_ordering(d);
+    struct node *it = list_first(ordering);
+    fprintf(stdout, "Ordererd graph: ");
     while (it != NULL) {
         struct Vertex *v = it->value;
         fprintf(stdout, "%d ", v->id);
 
         it = list_next(it);
-    }
+    }   
 
     fprintf(stdout, "\n");
 
-    dag_destroy_path(l);
+    dag_destroy_path(ordering);
     dag_destroy(d, false);
+    
 }
